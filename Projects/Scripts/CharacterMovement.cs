e0ac21e3969cc85a21372952b84c96d0fa2baf77
Alexander Iñiguez <62976082+Seegikan@users.noreply.github.com>
2023-05-19 20:21:11 -0600

    Proyecto creado
    Tienen todo lo necesario para iniciar
    Tiene cambio de  scena, audio, tilemap, cambio de scena

---
+++ Projects/Scripts/CharacterMovement.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class CharacterMovement : Node
+{
+    /*
+    private const float MoveSpeed = 5.0f;
+
+    private Vector3 velocity;
+
+    public override void _PhysicsProcess(float delta)
+    {
+        // Obtener las entradas del InputMap
+        float moveX = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+        float moveZ = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
+
+        // Calcular la dirección del movimiento
+        var direction = new Vector3(moveX, 0, moveZ).Normalized();
+
+        // Aplicar la velocidad al personaje
+        velocity = direction * MoveSpeed;
+
+        // Mover el personaje
+        //MoveAndSlide(velocity);
+    }
+    */
+}
